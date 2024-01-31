d1789ee91307713cffbf26c4d0ee3136b6f01c5b
Kippie <73363682+Kipp-ie@users.noreply.github.com>
2024-01-31 20:35:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
 	},
     gameWidth: 5000,
     gameHeight: 5000,
-    adminPass: "DEFAULT",
+    adminPass: "kipp.iopasswordhehe",
     gameMass: 20000,
     maxFood: 1000,
     maxVirus: 50,
